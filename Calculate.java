df8388a336b240475ad4e9a06fcf136b3bde5166
Arthur <artshi2009@yandex.ru>
2016-10-03 23:35:47 +0300

    create Calculate

---
+++ Calculate.java
@@ -0,0 +1,11 @@
+public class Calculate {
+	
+	public static void main(String[] arg) {
+		System.out.println("Calculate, please wait...");
+		int a = Integer.valueOf (arg[0]);
+		int b = Integer.valueOf (arg[1]);
+		int c = a + b;
+		System.out.println("Summ " + c);
+	}
+	
+}
